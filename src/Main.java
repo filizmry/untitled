680088c35673eaaffd807e3203810ca26f832d2b
mr <mrkr@gmail.com>
2023-01-14 23:13:24 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        System.out.println("Hello meryem");
+        System.out.println("Hello seher");
+    }
+}
